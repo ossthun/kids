4abc9c32a747ae6fabd69233178213dbbb0fe01b
ossthun <msline3@bluewin.ch>
2026-05-31 19:39:57 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kids Learning</title>
+
+        <link rel="icon" href="/icon.png" />
+
+        <link
+          rel="apple-touch-icon"
+          href="/icon.png"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
